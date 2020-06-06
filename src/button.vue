0e4f8a005bd4106fb0f9590e8f5a3a5f667e45c2
bycgxss <bycgxss@gmail.com>
2020-06-06 10:27:25 +0800

    添加 input.vue 及其样式

--- src/button.vue
+++ src/button.vue
@@ -10,6 +10,7 @@
 <script>
   import Icon from './icon'
   export default {
+    name: 'XssButton',
     components: {
       'g-icon': Icon
     },
@@ -29,7 +30,7 @@
     }
   }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
   @keyframes spin {
     0% {transform: rotate(0deg);}
     100% {transform: rotate(360deg);}
