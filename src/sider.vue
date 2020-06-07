6af58111349563b692cd8cf142298fe8452cf1ca
bycgxss <bycgxss@gmail.com>
2020-06-07 17:02:30 +0800

    基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'XssSider'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sider {
+
+  }
+</style>
