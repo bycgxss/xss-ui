74595466e019f5c4e9dcc652cb227b8dde15cc70
bycgxss <bycgxss@gmail.com>
2020-06-08 13:34:57 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Sider from './sider'
 import Footer from './footer'
 import Content from './content'
+import Toast from './toast'
+import Plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,9 @@ Vue.component('g-header', Header)
 Vue.component('g-sider', Sider)
 Vue.component('g-footer', Footer)
 Vue.component('g-content', Content)
+Vue.component('g-toast', Toast)
+
+Vue.use(Plugin)
 
 new Vue({
   el: '#app',
@@ -34,8 +39,8 @@ new Vue({
   created(){
   },
   methods: {
-    inputChange(e) {
-      console.log(e)
+    showToast() {
+      this.$toast('我是 msg')
     }
   }
 })
