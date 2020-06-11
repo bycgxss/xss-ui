625845789109432eea89ea072cffb83f814757f0
bycgxss <bycgxss@gmail.com>
2020-06-11 19:43:44 +0800

    更新 collapse 组件文档

---
+++ docs/.vuepress/components/collapse-demo1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <x-collapse :selected.sync="tabs" >
+      <x-collapse-item title="标题1" name="one">这是内容</x-collapse-item>
+      <x-collapse-item title="标题2" name="two">这是内容</x-collapse-item>
+      <x-collapse-item title="标题3"  name="three">这是内容</x-collapse-item>
+    </x-collapse>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+
+<script>
+  import Collapse from '../../../src/collapse'
+  import collapseItem from '../../../src/collapse-item'
+
+  export default {
+    name: "collapse-demo1",
+    data(){
+      return{
+        tabs:['one','two'],
+        content:`
+tabs:['one','two'],
+
+<x-collapse :selected.sync="tabs" >
+    <x-collapse-item title="标题1" name="one">这是内容</x-collapse-item>
+    <x-collapse-item title="标题2" name="two">这是内容</x-collapse-item>
+    <x-collapse-item title="标题3"  name="three">这是内容</x-collapse-item>
+</x-collapse>
+                `.trim()
+      }
+    },
+    components:{
+      'x-collapse':Collapse,
+      'x-collapse-item':collapseItem
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
