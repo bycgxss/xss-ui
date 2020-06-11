39353858ffe1ff48b0fe1027bc44c3351599c1aa
bycgxss <bycgxss@gmail.com>
2020-06-11 19:53:19 +0800

    更新 popover 组件文档

---
+++ docs/.vuepress/components/popover-demo1.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper">
+    <div class="block">
+      <x-popover trigger="click" position="left" class="popover">
+        <x-button>左</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="click" position="top" class="popover">
+        <x-button>上</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="click" position="bottom" class="popover">
+        <x-button>下</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="click" position="right" class="popover">
+        <x-button>右</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <pre><code>{{content}}</code></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Popover from '../../../src/popover'
+  import Button from '../../../src/button'
+  export default {
+    components: {
+      'x-popover': Popover,
+      'x-button': Button
+    },
+    data(){
+      return{
+        content: `
+<x-popover trigger="click" position="left" class="popover">
+    <x-button>左</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="click" position="top" class="popover">
+    <x-button>上</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="click" position="bottom" class="popover">
+    <x-button>下</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="click" position="right" class="popover">
+    <x-button>右</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+      `.trim()
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .wrapper{
+    .popover{
+      margin-left:20px;
+    }
+  }
+</style>
---
+++ docs/.vuepress/components/popover-demo2.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <div class="block">
+      <x-popover trigger="hover" position="left" class="popover">
+        <x-button>左</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="hover" position="top" class="popover">
+        <x-button>上</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="hover" position="bottom" class="popover">
+        <x-button>下</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <x-popover trigger="hover" position="right" class="popover">
+        <x-button>右</x-button>
+        <div slot="content">你好</div>
+      </x-popover>
+      <pre><code>{{content}}</code></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Popover from '../../../src/popover'
+  import Button from '../../../src/button'
+
+  export default {
+    components: {
+      'x-popover': Popover,
+      'x-button': Button
+    },
+    data() {
+      return {
+        content: `
+<x-popover trigger="hover" position="left" class="popover">
+    <x-button>左</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="hover" position="top" class="popover">
+    <x-button>上</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="hover" position="bottom" class="popover">
+    <x-button>下</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+<x-popover trigger="hover" position="right" class="popover">
+    <x-button>右</x-button>
+    <div slot="content">你好</div>
+</x-popover>
+      `.trim()
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    .popover {
+      margin-left: 20px;
+    }
+  }
+</style>
